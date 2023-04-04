8c29d446e6703a1a7d6d5bc2710c10d99fa38663
Daria-Misko <miskodaria.mail@gmail.com>
2023-04-04 19:22:12 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const conctacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(conctacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     const { contacts } = this.state;
     if (
@@ -51,23 +65,6 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('change');
-
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('change 2');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  componentDidMount() {
-    const conctacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(conctacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
 
   render() {
     const { filter } = this.state;
